04f96afdd567d29cbe7681a14028622ebb8c5d1d
Štefan Baebler <stefan.baebler@gmail.com>
2022-03-28 18:16:38 +0200

    No JSON key sorting

--- update.sh
+++ update.sh
@@ -17,11 +17,12 @@ jq -r 'map({id: .id, name: .name, address: .address, description: .description, 
 VolitveBASEURL="https://volitve.dvk-rs.si"
 mkdir -p volitve
 
-curl -s "${VolitveBASEURL}/config/config.json" | jq --sort-keys > volitve/config.json
-curl -s "${VolitveBASEURL}/data/obvestila.json"     | jq --sort-keys > volitve/obvestila.json
-curl -s "${VolitveBASEURL}/data/data.json"     | jq --sort-keys > volitve/data.json
-curl -s "${VolitveBASEURL}/data/liste.json"     | jq > volitve/liste.json
+curl -s "${VolitveBASEURL}/config/config.json"   | jq > volitve/config.json
+curl -s "${VolitveBASEURL}/data/obvestila.json"  | jq > volitve/obvestila.json
+curl -s "${VolitveBASEURL}/data/data.json"       | jq > volitve/data.json
+curl -s "${VolitveBASEURL}/data/liste.json"      | jq > volitve/liste.json
 jq -r '(.[0] | to_entries | map(.key)), (.[] | [.[]]) | @csv' volitve/liste.json > volitve/liste.csv
-curl -s "${VolitveBASEURL}/data/kandidati.json"     | jq --sort-keys > volitve/kandidati.json
-curl -s "${VolitveBASEURL}/data/zgod_udel.json"     | jq --sort-keys > volitve/zgod_udel.json
+curl -s "${VolitveBASEURL}/data/kandidati.json"  | jq > volitve/kandidati.json
+curl -s "${VolitveBASEURL}/data/zgod_udel.json"  | jq > volitve/zgod_udel.json
+
 
