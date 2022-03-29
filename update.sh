13a9010063f163cf9e6194be7d51f346294d282e
Štefan Baebler <stefan.baebler@gmail.com>
2022-03-29 09:42:12 +0200

    volišča: no key sorting, simplified conversion to CSV

--- update.sh
+++ update.sh
@@ -4,14 +4,14 @@ set -e
 BASEURL="https://www.dvk-rs.si/fileadmin/dvk_maps"
 mkdir -p dvk
 
-curl -s "${BASEURL}/notifications.json" | jq --sort-keys > dvk/notifications.json
-curl -s "${BASEURL}/settings.json"      | jq --sort-keys > dvk/settings.json
+curl -s "${BASEURL}/notifications.json" | jq > dvk/notifications.json
+curl -s "${BASEURL}/settings.json"      | jq > dvk/settings.json
 
-curl -s "${BASEURL}/volisca.csv.json"   | jq --sort-keys > dvk/volisca.csv.json
-jq -r 'map({id: .id, name: .name, address: .address, description: .description, lat: .lat, lon: .lon }) | (.[0] | to_entries | map(.key)), (.[] | [.[]]) | @csv' dvk/volisca.csv.json > dvk/volisca.csv
+curl -s "${BASEURL}/volisca.csv.json"   | jq > dvk/volisca.csv.json
+jq -r '(.[0] | to_entries | map(.key)), (.[] | [.[]]) | @csv' dvk/volisca.csv.json > dvk/volisca.csv
 
-curl -s "${BASEURL}/pg_volisca.csv.json"   | jq --sort-keys > dvk/pg_volisca.csv.json
-jq -r 'map({id: .id, name: .name, address: .address, description: .description, lat: .lat, lon: .lon }) | (.[0] | to_entries | map(.key)), (.[] | [.[]]) | @csv' dvk/pg_volisca.csv.json > dvk/pg_volisca.csv
+curl -s "${BASEURL}/pg_volisca.csv.json"   | jq > dvk/pg_volisca.csv.json
+jq -r '(.[0] | to_entries | map(.key)), (.[] | [.[]]) | @csv' dvk/pg_volisca.csv.json > dvk/pg_volisca.csv
 
 
 VolitveBASEURL="https://volitve.dvk-rs.si"
