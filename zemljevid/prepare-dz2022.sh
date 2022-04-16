e3c89d6aa299c1c18b7f4612b58932bc61039da7
Štefan Baebler <stefan.baebler@gmail.com>
2022-04-16 10:46:22 +0200

    Volišča OMNIA

---
+++ zemljevid/prepare-dz2022.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+geocodecsv -addressCol 5 -zipCol 6 -in volisca-omnia-dz2022.csv -out volisca-omnia-dz2022-geocoded.csv
+rm volisca-omnia-dz2022-tocke.geojson
+ogr2ogr -f "GeoJSON" volisca-omnia-dz2022-tocke.geojson volisca-omnia-dz2022-geocoded.csv \
+	-a_srs 'EPSG:4326' \
+	-oo X_POSSIBLE_NAMES=lon \
+	-oo Y_POSSIBLE_NAMES=lat \
+	-oo KEEP_GEOM_COLUMNS=NO \
+	-lco RFC7946=YES -lco WRITE_BBOX=YES \
+	-nln VDV-GURS-RPE-DVK-Predcasno-tocke
+
+echo "  done."
