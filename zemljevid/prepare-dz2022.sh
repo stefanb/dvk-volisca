5fcc7be35369bc4e1af63dd35956536f3d663cbc
Štefan Baebler <stefan.baebler@gmail.com>
2022-04-16 11:20:59 +0200

    Volišča za predčasno glasovanje - točke
    2022/04/16 11:19:53 Geocoded 100.00% (96/96) addresses in 19.548217ms. 0 errors.

--- zemljevid/prepare-dz2022.sh
+++ zemljevid/prepare-dz2022.sh
@@ -10,4 +10,14 @@ ogr2ogr -f "GeoJSON" volisca-omnia-dz2022-tocke.geojson volisca-omnia-dz2022-geo
 	-lco RFC7946=YES -lco WRITE_BBOX=YES \
 	-nln VDV-GURS-RPE-DVK-OMNIA-tocke
 
+geocodecsv -addressCol 9 -zipCol 10 -in volisca-predcasno-dz2022.csv -out volisca-predcasno-dz2022-geocoded.csv
+rm volisca-predcasno-dz2022-tocke.geojson
+ogr2ogr -f "GeoJSON" volisca-predcasno-dz2022-tocke.geojson volisca-predcasno-dz2022-geocoded.csv \
+	-a_srs 'EPSG:4326' \
+	-oo X_POSSIBLE_NAMES=lon \
+	-oo Y_POSSIBLE_NAMES=lat \
+	-oo KEEP_GEOM_COLUMNS=NO \
+	-lco RFC7946=YES -lco WRITE_BBOX=YES \
+	-nln VDV-GURS-RPE-DVK-Predcasno-tocke
+
 echo "  done."
