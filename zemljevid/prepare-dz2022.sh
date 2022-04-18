532f4931d3a0c60c5ad8b53a31541b9ab40fe4a5
Štefan Baebler <stefan.baebler@gmail.com>
2022-04-18 04:05:17 +0200

    Predčasna volišča: Poligoni področij VO

--- zemljevid/prepare-dz2022.sh
+++ zemljevid/prepare-dz2022.sh
@@ -20,4 +20,21 @@ ogr2ogr -f "GeoJSON" volisca-predcasno-dz2022-tocke.geojson volisca-predcasno-dz
 	-lco RFC7946=YES \
 	-nln VDV-GURS-RPE-DVK-Predcasno-tocke
 
+# Poligoni
+# curl -o VDV-GURS-RPE.geojson https://raw.githubusercontent.com/stefanb/gurs-rpe/master/data/VDV.geojson
+# curl -o OB-GURS-RPE.geojson https://raw.githubusercontent.com/stefanb/gurs-rpe/master/data/OB.geojson
+
+rm volisca-predcasno-dz2022-poligoni.geojson
+ogr2ogr volisca-predcasno-dz2022-poligoni.geojson VDV-GURS-RPE.geojson -dialect sqlite \
+ -sql "SELECT ST_Union(geometry),
+		GROUP_CONCAT('- '|| dvk.OVK || ': ' || OVK_ime, char(10)) as ovk,
+		dvk.sedez as 'name',
+		dvk.sedez_naslov as 'address',
+		dvk.sedez_posta as 'city'
+	FROM 'VDV' AS src
+		LEFT JOIN 'volisca-predcasno-dz2022.csv'.volisca-predcasno-dz2022 AS dvk ON cast(src.VDV_ID as text)=dvk.OVK
+    WHERE src.ENOTA = 'VO'
+    GROUP BY dvk.sedez, dvk.sedez_naslov, dvk.sedez_posta"\
+ -nln VDV-GURS-RPE-DVK-Predcasno -overwrite
+
 echo "  done."
